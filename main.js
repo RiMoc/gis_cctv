c8e7cdf775a5e300f0a82d580fcf1432c7e587b5
RiMoc <surin.2543.matharin@gmail.com>
2023-11-27 15:20:58 +0700

    Up

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-const express = require("express")
-
+const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
-
 app.set('view engine', 'ejs')
-
 app.get('/', (req, res) => res.render('index', { code: "NSW01" }))
 app.get('/:code', (req, res) => res.render('index', { code: req.params.code }) )
-
 app.listen(port, () => console.log(`\x1b[36m[ INFO ]\x1b[0m Server is Running !`))
-module.exports = app
