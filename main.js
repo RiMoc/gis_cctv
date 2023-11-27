aacda14cc0c6b56db72d999946d70539c81ab7ed
RiMoc <surin.2543.matharin@gmail.com>
2023-11-27 15:30:10 +0700

    change

--- main.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8080
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('index', { code: "NSW01" }))
-app.get('/:code', (req, res) => res.render('index', { code: req.params.code }) )
-app.listen(port, () => console.log(`\x1b[36m[ INFO ]\x1b[0m Server is Running !`))
